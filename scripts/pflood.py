f61f68d128dd7a193467a8f95fac5b1d06126daf
Ariel Ril <ariel.ril@4all.com>
2020-05-11 01:53:19 -0300

    add frequency control for  icmp packets

--- scripts/pflood.py
+++ scripts/pflood.py
@@ -19,36 +19,61 @@ ETH_P_IP = 0x0800
 
 IP_PROTO_ICMP = 1
 
-# TODO save the frequency of package receiving from each IP
 ip_dictionary = dict({})
 
 
-# TODO check this computation if it is ok
-def computeFrequency(ip: str) -> int:
-    now = dt.now()
-    lastTime = ip_dictionary[ip]['lastTime']
-    elapsed = now - lastTime
+def bytes_to_mac(bytesmac):
+    return ":".join("{:02x}".format(x) for x in bytesmac)
+
+
+def computeFrequency(ip: str) -> float:
     pktCount = ip_dictionary[ip]['pktCount']
-    return pktCount / elapsed.total_seconds()
+    elapsedOcc = (dt.now() - ip_dictionary[ip]['lastOcc']).total_seconds()
+
+    # compute the frequency with packet per second
+    return pktCount / elapsedOcc
 
 
-def addFoundIPAndCalcFreq(ip: str):
+def addFoundIP(ip: str) -> bool:
     if ip not in ip_dictionary.keys():
-        ip_dictionary[ip] = {
-            'lastTime': dt.now(),
-            'frequency': 0,
-            'pktCount': 1
-        }
-    else:
-        ip_dictionary[ip]['lastTime'] = dt.now()
-        ip_dictionary[ip]['pktCount'] += 1
-
-# TODO verify if the pkt count must be resetted here
+        ip_dictionary[ip] = dict({
+            'pktCount': 0,
+        })
+        return True
+    return False
 
 
-def updatePktFrequency():
+def incrementReceivedPackets(ip: str):
+    # create the property
+    if 'pktCount' not in ip_dictionary[ip].keys():
+        ip_dictionary[ip]['pktCount'] = 0
+
+    # increment pktCount
+    ip_dictionary[ip]['pktCount'] += 1
+
+    # if it is the first packet, set the occurrence time
+    if ip_dictionary[ip]['pktCount'] == 1:
+        ip_dictionary[ip]['lastOcc'] = dt.now()
+        print('pkt one', ip_dictionary[ip]['lastOcc'])
+
+    # if the pktCount passes the threshold, compute frequency and clear pktCount
+    if ip_dictionary[ip]['pktCount'] > 2:
+        updatePktFrequency(ip)
+        ip_dictionary[ip]['pktCount'] = 0
+
+
+def updatePktFrequency(ip: str):
+    ip_dictionary[ip]['frequency'] = computeFrequency(ip)
+
+
+def updateAllPktFrequency():
     for ip in ip_dictionary.keys():
-        ip_dictionary[ip]['frequency'] = computeFrequency(ip)
+        updatePktFrequency(ip)
+
+
+def cleanAllPacketCount():
+    for ip in ip_dictionary.keys():
+        ip_dictionary[ip]['pktCount'] = 0
 
 
 def getSocket(if_net: str) -> socket:
@@ -56,7 +81,7 @@ def getSocket(if_net: str) -> socket:
         s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW,
                           socket.ntohs(ETH_P_ALL))
     except OSError as msg:
-        print('Error'+str(msg))
+        print('failed to create socket', str(msg))
         sys.exit(1)
 
     print('Socket created!')
@@ -78,6 +103,17 @@ def isIcmpRep(tp: int, code: int) -> bool:
     return tp == 0 and code == 0
 
 
+def printIPFreq():
+    for ip in ip_dictionary.keys():
+        freq = 0
+        pktCount = 0
+        if 'frequency' in ip_dictionary[ip].keys():
+            freq = ip_dictionary[ip]['frequency']
+        if 'pktCount' in ip_dictionary[ip].keys():
+            pktCount = ip_dictionary[ip]['pktCount']
+        print('IP:', ip, '\tFreq:', freq, '\tCount:', pktCount)
+
+
 if __name__ == "__main__":
     sock = getSocket('eth0')
 
@@ -93,13 +129,7 @@ if __name__ == "__main__":
 
         packet_type = ether[2]
 
-        # compute the frequency for each known host
-        elapsedExec = dt.now() - lastExec
-        if elapsedExec.total_seconds() >= 5:
-            updatePktFrequency()
-
         if isIP(packet_type):
-            print('is IP')
             ip_unpack = struct.unpack(
                 '!BBHHHBBH4s4s', packet[eth_len:20+eth_len])
 
@@ -107,17 +137,30 @@ if __name__ == "__main__":
             ip_source = socket.inet_ntoa(ip_unpack[8])
             ip_dest = socket.inet_ntoa(ip_unpack[9])  # * this is me!
 
+            # protocol encapsulated inside the IP
+            ip_protocol = ip_unpack[6]
+            # options for the encapsulated protocol
             ip_options_type = ip_options[0]
             ip_options_code = ip_options[1]
 
-            ip_protocol = ip_unpack[6]
-            print('ip proto', ip_protocol)
-
             if ip_protocol == IP_PROTO_ICMP:
+                print('ping')
                 if isIcmpReq(ip_options_type, ip_options_code):
-                    print('icmp req')
-                    addFoundIPAndCalcFreq(ip_source)
-                    print('ip dict', ip_dictionary)
+                    """
+                        When receiving an ICMP Request (ping) we have to save the source 
+                        machines to use to counter attack the attacker that is using 
+                        ping flood.
+                        While receiving valid icmp requests is needed to compute the 
+                        frequency of the ping execution from the ip_source to check if the 
+                        source isn't executing a ping flood attack.
+                        If the ip_source is attacking the home machine, we need to counter attack!
+                    """
+                    if addFoundIP(ip_source):
+                        print('MAC:', bytes_to_mac(ether[1]))
+                        print('IP:', ip_source)
 
-        lastExec = dt.now()
-        print('\n')
+                    incrementReceivedPackets(ip_source)
+
+            printIPFreq()
+
+        # print('\n')
